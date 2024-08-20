e444d583b651db63332162ed8277aa58c099c0eb
DapowerfulAli <alizpower9900@hotmail.com>
2024-08-20 18:11:00 +0600

    added a js script

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let usrAge = prompt("How old are you?");
+if (usrAge >= 18 && usrAge <= 65){
+    console.log('Welcome to this construction zone.');
+} else if (usrAge > 65){
+    console.log('You have very little time left to be wasting here.');
+} else {
+    console.log('what are you doing here! you are not even allowed to vote.');
+}
